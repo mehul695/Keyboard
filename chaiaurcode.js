037087f0b3d604d78fefab974fb863f93869f981
Mehul <you@example.com>
2023-09-08 16:31:27 +0530

    initial commit

---
+++ chaiaurcode.js
@@ -0,0 +1,24 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown",function(e){
+    insert.innerHTML = `
+        <div class = "color"> 
+
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.Code}</td>
+  </tr>
+ 
+</table>
+
+</div>
+
+`
+})
